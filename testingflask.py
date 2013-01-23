3111a3b85bfe9ee5d2deca4d1074c4e6d497f0c9
siddtewari <sidtewari@gmail.com>
2013-01-23 17:47:13 -0500

    Tested Flask

--- testingflask.py
+++ testingflask.py
@@ -1,2 +1,12 @@
 # This is where you testing flask exercise goes
-# Name: 
+# Name: Sidd Tewari
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return "Hello Internet!"
+
+if __name__ == "__main__":
+	app.run()
