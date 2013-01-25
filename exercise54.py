940241d75192bcd5659bbbe58b7fe45f5c973403
siddtewari <sidtewari@gmail.com>
2013-01-25 08:21:54 -0500

    modified answer 2 for non positive values

--- exercise54.py
+++ exercise54.py
@@ -4,10 +4,13 @@
 # Triangles
 
 def is_triangle(a,b,c):
-	if (a > (b + c)) or (b > (c + a)) or (c > (a + b)):
-		print 'No'
+	if (a <= 0 or b <= 0 or c <= 0):
+		print 'Please enter only positive numbers'
 	else:
-		print 'Yes'
+		if (a > (b + c)) or (b > (c + a)) or (c > (a + b)):
+			print 'No'
+		else:
+			print 'Yes'
 
 #is_triangle(1,2,3)
 #is_triangle(8,2,1)
@@ -18,7 +21,8 @@ def get_lengths():
 	x = input("Please enter the first side of the triangle: ")
 	y = input("Please enter the second side of the triangle: ")
 	z = input("Please enter the third side of the triangle: ")
-	is_triangle(x,y,z)
+	is_triangle(int(x),int(y),int(z))
+	#is_triangle(x,y,z)
 
 get_lengths()
 
