96cede1b67f525a4228b95b645f5a26f50380772
siddtewari <sidtewari@gmail.com>
2013-01-24 20:04:09 -0500

    Solved 5.4 - 1

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,15 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name: Sidd Tewari
+
+# Triangles
+
+def is_triangle(a,b,c):
+	if (a > (b + c)) or (b > (c + a)) or (c > (a + b)):
+		print 'No'
+	else:
+		print 'Yes'
+
+is_triangle(1,2,3)
+is_triangle(8,2,1)
+is_triangle(3,4,7)
+is_triangle(3,4,8)
